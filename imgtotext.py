278d53b37fdc32931a87873c05e487900c6b1e19
harshit <harshitsaamu@gmail.com>
2018-02-05 01:12:50 +0530

    updated

--- imgtotext.py
+++ imgtotext.py
@@ -7,8 +7,10 @@ import time
 while(True):
     time.sleep(1)
     response = requests.get("http://192.168.43.1:8080/photo.jpg")
-    # print( type(response) ) # <class 'requests.models.Response'>
     img = Image.open(io.BytesIO(response.content))
-    # print( type(img) ) # <class 'PIL.JpegImagePlugin.JpegImageFile'>
     text = pytesseract.image_to_string(img)
-    print( text )
+    a='stop'
+    if(a in text):
+        print('stop')
+    else:
+        print('run')    
